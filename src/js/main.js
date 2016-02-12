9bf0daa55d789b7c4668ded11a973f856bf1a7ec
CharlesHurt <charles@charleshurt.com>
2016-02-11 21:48:17 -0800

    Got a route working

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,28 @@
 
  var app = angular.module('S-O-App', ['ui.router'])
 
- app.controller("userController", function($scope) {
-  $scope.x = 42//
-  //$scope.go('/showLogin')
- })
-
+// Put this config before defining the controllers
  app.config(function($stateProvider, $urlRouterProvider) {
+
+   console.log($stateProvider);
    $stateProvider
-     .state('home', {url: '/', templateUrl: './partials/home.html'})
-     .state('login', {url: '/login', templateUrl: './partials/login.html'})
+     .state('home', {url: '/home', templateUrl: 'partials/home.html', controller:"userController"} )
+     .state('login'/*simple name no slash */,
+       {url: '/login' /* what the user would see on URL */,
+       templateUrl: 'partials/login.html',
+       controller:"userController"})
 
    $urlRouterProvider.otherwise('/')
+ })
+
+// This is after .config, but not often used
+//app.run...
+
+ app.controller("userController", function($scope, $state) {
+  $scope.x = 42
+  $scope.gotoLogin = function() {
+    $state.go("login")
+    console.log("transition done");
+  }
+  //$scope.go('/showLogin')
  })
