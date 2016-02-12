16fc0798c45c1284bdd9541f3ed7b5483e551690
CharlesHurt <charles@charleshurt.com>
2016-02-11 22:10:46 -0800

    All routes working

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,29 @@
 
    console.log($stateProvider);
    $stateProvider
-     .state('home', {url: '/home', templateUrl: 'partials/home.html', controller:"userController"} )
-     .state('login'/*simple name no slash */,
+     .state('homeState',
+       {url: '/home',
+       templateUrl: 'partials/home.html',
+       controller:"userController"})
+     .state('loginState'/*simple name no slash */,
        {url: '/login' /* what the user would see on URL */,
        templateUrl: 'partials/login.html',
+       controller:"userController"})
+     .state('registerState'/*simple name no slash */,
+       {url: '/register' /* what the user would see on URL */,
+       templateUrl: 'partials/register.html',
+       controller:"userController"})
+     .state('profileState'/*simple name no slash */,
+       {url: '/profile' /* what the user would see on URL */,
+       templateUrl: 'partials/profile.html',
+       controller:"userController"})
+     .state('browseState'/*simple name no slash */,
+       {url: '/browse' /* what the user would see on URL */,
+       templateUrl: 'partials/browse.html',
+       controller:"userController"})
+     .state('chatState'/*simple name no slash */,
+       {url: '/chat' /* what the user would see on URL */,
+       templateUrl: 'partials/chat.html',
        controller:"userController"})
 
    $urlRouterProvider.otherwise('/')
@@ -20,10 +39,23 @@
 //app.run...
 
  app.controller("userController", function($scope, $state) {
-  $scope.x = 42
-  $scope.gotoLogin = function() {
-    $state.go("login")
-    console.log("transition done");
+  //$scope.x = 42
+  $scope.gotoHomeState = function() {
+    $state.go("homeState")
   }
-  //$scope.go('/showLogin')
+  $scope.gotoLoginState = function() {
+    $state.go("loginState")
+  }
+  $scope.gotoRegisterState = function() {
+    $state.go("registerState")
+  }
+  $scope.gotoProfileState = function() {
+    $state.go("profileState")
+  }
+  $scope.gotoBrowseState = function() {
+    $state.go("browseState")
+  }
+  $scope.gotoChatState = function() {
+    $state.go("chatState")
+  }
  })
